1c01b7aa54b8001caabc830e5242a36bb5a8ac0c
athifah_brilianty_nurul_falah <athifahbrilianty@gmail.com>
2021-03-10 14:27:01 +0700

    Menambahkan jenis hewan dan mengubah layout

--- app/src/main/java/org/d3if4012/galerihewan/Hewan.kt
+++ app/src/main/java/org/d3if4012/galerihewan/Hewan.kt
@@ -3,5 +3,6 @@ package org.d3if4012.galerihewan
 data class Hewan(
     val nama : String,
     val namaLatin : String,
-    val imageResId : Int
+    val imageResId : Int,
+    val jenisHewan : String
 )
--- app/src/main/java/org/d3if4012/galerihewan/MainAdapter.kt
+++ app/src/main/java/org/d3if4012/galerihewan/MainAdapter.kt
@@ -14,6 +14,7 @@ class MainAdapter(private val data: List<Hewan>) :
             namaTextView.text = hewan.nama
             latinTextView.text = hewan.namaLatin
             imageView.setImageResource(hewan.imageResId)
+            jenisHewanTextView.text = hewan.jenisHewan
         }
     }
 
