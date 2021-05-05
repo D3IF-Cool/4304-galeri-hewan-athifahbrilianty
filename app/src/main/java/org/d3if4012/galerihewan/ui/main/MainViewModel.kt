0ffef7abe2245fbe985185e1adebeead43f91cb6
athifah_brilianty_nurul_falah <athifahbrilianty@gmail.com>
2021-05-05 17:59:22 +0700

    3.3.Mengambil data dari internet

--- app/src/main/java/org/d3if4012/galerihewan/ui/main/MainViewModel.kt
+++ app/src/main/java/org/d3if4012/galerihewan/ui/main/MainViewModel.kt
@@ -1,17 +1,34 @@
 package org.d3if4012.galerihewan.ui.main
 
+import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.launch
 import org.d3if4012.galerihewan.R
 import org.d3if4012.galerihewan.model.Hewan
+import org.d3if4012.galerihewan.network.HewanApi
 
 class MainViewModel : ViewModel(){
 
     private val data = MutableLiveData<List<Hewan>>()
     init {
         data.value = initData()
+        retrieveData()
     }
+
+    private fun retrieveData() {
+        viewModelScope.launch {
+            try {
+                val result = HewanApi.service.getHewan()
+                Log.d("MainViewModel", "Success: $result")
+            } catch (e: Exception) {
+                Log.d("MainViewModel", "Failure: ${e.message}")
+            }
+        }
+    }
+
 
     private fun initData(): List<Hewan> {
         return listOf(
